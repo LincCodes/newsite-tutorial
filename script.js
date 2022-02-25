ab5f46184dd8d85f8e3b6376dc2109e66a7855d4
LincCodes <78691632+LincCodes@users.noreply.github.com>
2022-02-25 08:16:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector("#menu");
+const items = document.querySelector("#items");
+const body = document.querySelector("body");
+
+menu.addEventListener("click", show);
+items.addEventListener("click", hide);
+body.addEventListener("click", hide, true);
+
+function show() {
+	items.classList.add("show");
+}
+function hide() {
+	items.classList.remove("show");
+}
